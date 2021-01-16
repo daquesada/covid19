b6599fc4d6eaa4ad4bf0eb3e8559022fa4347d19
Daniel <daquesada@unicauca.edu.co>
2021-01-16 18:22:25 -0500

    activate service worker :bee:

--- src/Componets/Card/Card.js
+++ src/Componets/Card/Card.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Card.css'
+import React from "react";
+import "./Card.css";
 // style="width: 18rem;"
 export function Card(props) {
-    const color = props.color
-    return (
-        <div className="card" >
-            <div className="card-body">
-                <h4 className="card-title text-muted text-center" >{props.title}</h4>
-                <h2 className="card-subtitle mb-2 text-center" style={{ color: color }}>{props.number}</h2>
-                {props.table}
-                {props.children}
-            </div>
-        </div>
-    )
+  const color = props.color;
+  return (
+    <div className={"card " + props.className}>
+      <div className="card-body">
+        <h4 className="card-title text-muted text-center">{props.title}</h4>
+        <h2 className="card-subtitle mb-2 text-center" style={{ color: color }}>
+          {props.number}
+        </h2>
+        {props.table}
+        {props.children}
+      </div>
+    </div>
+  );
 }
--- src/Componets/Main/Main.js
+++ src/Componets/Main/Main.js
@@ -1,41 +1,57 @@
-import React from 'react'
-import {Card} from '../Card/Card';
-import Table from '../Table/Table';
-import { useCountry } from '../../Hooks/useCountry'
-import Loading from '../Loading/Loading'
+import React from "react";
+import { Card } from "../Card/Card";
+import Table from "../Table/Table";
+import { useCountry } from "../../Hooks/useCountry";
+import Loading from "../Loading/Loading";
+import "./styles.css";
 
 export default function Main() {
-    const { getStats, loading} = useCountry();
-    
-    if(loading){
-        return(
-            <div className="mx-auto mt-auto text-center">
-                <Loading />
-            </div>
-        )
-    }
+  const { getStats, loading } = useCountry();
 
+  if (loading) {
     return (
-        <div>
-            <div className='row'>
-                <div className='col col-md-4 mt-3'>
-                    {/* #e9ec1e */}
-                    <Card title='Confirmed' color='#fa983a' number={new Intl.NumberFormat().format(getStats().confirmed)} />
-                </div>
-                <div className='col-md-4 mt-3'>
-                    <Card title='Recovered' color="#4cff16" number={new Intl.NumberFormat().format(getStats().recovered)} />
-                </div>
-                <div className='col-md-4 mt-3'>
-                    <Card title='Deaths' color='#ff3b3b' number={new Intl.NumberFormat().format(getStats().deaths)} />
-                </div>
-            </div>
-            <div className="row mt-3" >
-                <div className="col">
-                    <Card title={'Reported cases by country'}>
-                        <Table />
-                    </Card>
-                </div>
-            </div>
+      <div className="mx-auto mt-auto text-center">
+        <Loading />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="row">
+        <div className="col col-md-4 mt-3 ">
+          {/* #e9ec1e */}
+          <Card
+            className="hover-card"
+            title="Confirmed"
+            color="#fa983a"
+            number={new Intl.NumberFormat().format(getStats().confirmed)}
+          />
         </div>
-    )
+        <div className="col-md-4 mt-3 ">
+          <Card
+            className="hover-card"
+            title="Recovered"
+            color="#4cff16"
+            number={new Intl.NumberFormat().format(getStats().recovered)}
+          />
+        </div>
+        <div className="col-md-4 mt-3">
+          <Card
+            className="hover-card"
+            title="Deaths"
+            color="#ff3b3b"
+            number={new Intl.NumberFormat().format(getStats().deaths)}
+          />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col">
+          <Card title={"Reported cases by country"}>
+            <Table />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
