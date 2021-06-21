b1e138055ec003b56e74935c45befd8dcb02daad
daquesada <daquesada@unicauca.edu.co>
2021-06-21 12:03:32 -0500

    fix cache-first SW :bee:

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = "static-v1";
-const DYNAMIC_CACHE = "dynamic-v1";
+const STATIC_CACHE = "static-v2";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
@@ -16,15 +16,27 @@ const APP_SHELL = [
   // "static/js/runtime-main.2f9f3538.js",
   // "static/js/runtime-main.2f9f3538.js.map",
 
-  "https://corona-api.com/countries",
+  // "https://corona-api.com/countries",
 ];
 
 const APP_SHELL_INMUTABLE = ["static/css/2.86846cc5.chunk.css"];
+
+function clearCache(cacheName, numeroItems) {
+  caches.open(cacheName).then((cache) => {
+    return cache.keys().then((keys) => {
+      if (keys.length > numeroItems) {
+        // eslint-disable-next-line no-unused-vars
+        cache.delete(keys[0]).then(clearCache(cacheName, numeroItems));
+      }
+    });
+  });
+}
 
 function updateDynamicCache(dynamicCache, request, response) {
   if (response.ok) {
     return caches.open(dynamicCache).then((cache) => {
       cache.put(request, response.clone());
+      clearCache(dynamicCache, 50);
       return response.clone();
     });
   } else {
@@ -47,8 +59,10 @@ self.addEventListener("install", (e) => {
 
   e.waitUntil(Promise.all([cacheInmutable, cacheStatic]));
 });
+
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener("activate", (e) => {
+  //clean up cache storage
   const response = caches.keys().then((keys) => {
     keys.forEach((key) => {
       if (key !== STATIC_CACHE && key.includes("static")) {
@@ -65,16 +79,34 @@ self.addEventListener("activate", (e) => {
 
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener("fetch", (e) => {
-  const response = caches.match(e.request).then((res) => {
-    let response = null;
-    if (res) {
-      response = res;
-    } else {
-      response = fetch(e.request).then((dynamicRes) =>
-        updateDynamicCache(DYNAMIC_CACHE, e.request, dynamicRes)
-      );
-    }
-    return response;
-  });
+  let response = null;
+  //Network first if there is a request to the API
+  if (e.request.url.includes("corona-api")) {
+    response = fetch(e.request)
+      .then((res) => {
+        if (!res) return caches.match(e.request);
+
+        caches.open(DYNAMIC_CACHE).then((cache) => {
+          cache.put(e.request, res);
+          clearCache(DYNAMIC_CACHE, 50);
+        });
+
+        return res.clone();
+      })
+      .catch(() => caches.match(e.request));
+  } else {
+    //Cache first network fallback
+    response = caches.match(e.request).then((resCache) => {
+      let res = null;
+      if (resCache) {
+        res = resCache;
+      } else {
+        res = fetch(e.request).then((dynamicRes) =>
+          updateDynamicCache(DYNAMIC_CACHE, e.request, dynamicRes)
+        );
+      }
+      return res;
+    });
+  }
   e.respondWith(response);
 });
